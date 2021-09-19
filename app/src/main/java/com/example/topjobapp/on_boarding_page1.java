54f2de98eff599fd2b132c203f2de97665ffd8ca
Minuli-Lavanya <minulilavanya@gmail.com>
2021-09-19 22:59:13 +0530

    initial commit

---
+++ app/src/main/java/com/example/topjobapp/on_boarding_page1.java
@@ -0,0 +1,14 @@
+package com.example.topjobapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class on_boarding_page1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_page1);
+    }
+}
