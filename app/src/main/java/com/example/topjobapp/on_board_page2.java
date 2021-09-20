51ec90511c7e6d06738f7bbd929611aa371db038
Minuli-Lavanya <minulilavanya@gmail.com>
2021-09-20 18:21:12 +0530

    add on board page 2

---
+++ app/src/main/java/com/example/topjobapp/on_board_page2.java
@@ -0,0 +1,14 @@
+package com.example.topjobapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class on_board_page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_board_page2);
+    }
+}
