d9e44ade4a393496278c7e75cb00137d73fc8c1b
Minuli-Lavanya <minulilavanya@gmail.com>
2021-09-29 23:40:35 +0530

    Add job provider login

---
+++ app/src/main/java/com/example/topjobapp/JobProvider_login.java
@@ -0,0 +1,14 @@
+package com.example.topjobapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobProvider_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_provider_login);
+    }
+}
