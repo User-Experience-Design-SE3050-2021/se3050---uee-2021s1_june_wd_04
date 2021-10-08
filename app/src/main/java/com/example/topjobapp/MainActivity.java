98287962f577e03251ff8f90de147e6ed6ac1de9
Minuli-Lavanya <minulilavanya@gmail.com>
2021-10-09 00:47:43 +0530

    Add intents

--- app/src/main/java/com/example/topjobapp/MainActivity.java
+++ app/src/main/java/com/example/topjobapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.topjobapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcome = findViewById(R.id.u_tapbtn1);
+
+        welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, on_boarding_page1.class));
+            }
+        });
     }
 }
