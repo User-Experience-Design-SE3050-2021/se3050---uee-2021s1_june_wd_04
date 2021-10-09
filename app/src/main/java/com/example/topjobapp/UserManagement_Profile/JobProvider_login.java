09173d1f822773d4cf36e14930923b07b05a861a
Lakma <lakmagamage97@gmail.com>
2021-10-09 13:42:13 +0530

    Add home page and new job vacancy page

--- app/src/main/java/com/example/topjobapp/UserManagement_Profile/JobProvider_login.java
+++ app/src/main/java/com/example/topjobapp/UserManagement_Profile/JobProvider_login.java
@@ -2,15 +2,36 @@ package com.example.topjobapp.UserManagement_Profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.topjobapp.JobProvider_home;
 import com.example.topjobapp.R;
 
 public class JobProvider_login extends AppCompatActivity {
+
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_provider_login);
+
+
+
+
+        button1 = findViewById(R.id.u_button9);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JobProvider_login.this, JobProvider_home.class));
+            }
+        });
     }
+
+
 }
