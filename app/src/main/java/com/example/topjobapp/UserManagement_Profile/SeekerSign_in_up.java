96b6c96ff181ebfd8a39828e301775aadb9e8679
Minuli-Lavanya <minulilavanya@gmail.com>
2021-10-09 10:21:41 +0530

    Add button intents

--- app/src/main/java/com/example/topjobapp/UserManagement_Profile/SeekerSign_in_up.java
+++ app/src/main/java/com/example/topjobapp/UserManagement_Profile/SeekerSign_in_up.java
@@ -2,15 +2,38 @@ package com.example.topjobapp.UserManagement_Profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.topjobapp.R;
 
 public class SeekerSign_in_up extends AppCompatActivity {
 
+    private Button signin, signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seeker_sign_in_up);
+
+        signin = findViewById(R.id.u_logins);
+        signup = findViewById(R.id.u_regs);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SeekerSign_in_up.this, JobSeeker_login.class));
+
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SeekerSign_in_up.this, Job_Seeker_Register.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/topjobapp/UserManagement_Profile/provider_sign_in_up.java
+++ app/src/main/java/com/example/topjobapp/UserManagement_Profile/provider_sign_in_up.java
@@ -2,15 +2,37 @@ package com.example.topjobapp.UserManagement_Profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.topjobapp.R;
 
 public class provider_sign_in_up extends AppCompatActivity {
 
+    private Button login, signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_sign_in_up);
+
+        login = findViewById(R.id.u_loginp);
+        signup = findViewById(R.id.u_regp);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(provider_sign_in_up.this, JobProvider_login.class));
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(provider_sign_in_up.this, Job_Provider_Register.class));
+            }
+        });
     }
 }
