3c2ce46a96054885fd260fcc435238e3a4033559
Minuli-Lavanya <minulilavanya@gmail.com>
2021-10-10 22:44:49 +0530

    add job provider login implementation

---
+++ app/src/main/java/com/example/topjobapp/Model/JobProvider.java
@@ -0,0 +1,67 @@
+package com.example.topjobapp.Model;
+
+public class JobProvider {
+
+    private String username, email, phone, cname, address, password;
+
+    JobProvider(){
+
+    }
+
+    public JobProvider(String username, String email, String phone, String cname, String address, String password) {
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.cname = cname;
+        this.address = address;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
