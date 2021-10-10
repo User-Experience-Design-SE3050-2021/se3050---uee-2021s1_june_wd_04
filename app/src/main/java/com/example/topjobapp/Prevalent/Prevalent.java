89eae792a06a90212e290587112c3e115cb421f7
Minuli-Lavanya <minulilavanya@gmail.com>
2021-10-10 11:58:38 +0530

    Job seeker login implement

---
+++ app/src/main/java/com/example/topjobapp/Prevalent/Prevalent.java
@@ -0,0 +1,8 @@
+package com.example.topjobapp.Prevalent;
+
+import com.example.topjobapp.Model.JobSeeker;
+
+public class Prevalent {
+
+    public static JobSeeker currentOnlineUser;
+}
--- app/src/main/java/com/example/topjobapp/UserManagement_Profile/JobSeeker_login.java
+++ app/src/main/java/com/example/topjobapp/UserManagement_Profile/JobSeeker_login.java
@@ -1,16 +1,109 @@
 package com.example.topjobapp.UserManagement_Profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.topjobapp.Model.JobSeeker;
+import com.example.topjobapp.Prevalent.Prevalent;
 import com.example.topjobapp.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class JobSeeker_login extends AppCompatActivity {
+
+    private TextInputLayout name, pass;
+    private Button login;
+
+    private String parentDbName = "JobSeeker";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_seeker_login);
+
+        name = findViewById(R.id.u_uname);
+        pass = findViewById(R.id.u_upass);
+
+        login = findViewById(R.id.u_button9);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginUser();
+            }
+        });
+
+    }
+
+    private void LoginUser() {
+
+        String t_name = name.getEditText().getText().toString().trim();
+        String t_password = pass.getEditText().getText().toString().trim();
+
+        if(TextUtils.isEmpty(t_name) || TextUtils.isEmpty(t_password)){
+            Toast.makeText(this, "Please fill the fields", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+            LoginValidation(t_name, t_password);
+
+
+        }
+
+    }
+
+    private void LoginValidation(String t_name, String t_password) {
+
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+
+                if(datasnapshot.child(parentDbName).child(t_name).exists())
+                {
+                    JobSeeker userData = datasnapshot.child(parentDbName).child(t_name).getValue(JobSeeker.class);
+
+                    if(userData.getUsername().equals(t_name))
+                    {
+                        if(userData.getPassword().equals(t_password))
+                        {
+                            Toast.makeText(JobSeeker_login.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(JobSeeker_login.this, JobSeeker_login.class);
+                            startActivity(intent);
+
+                            Prevalent.currentOnlineUser = userData;
+                        }
+                        else{
+                            Toast.makeText(JobSeeker_login.this, "email or password incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else {
+                    Toast.makeText(JobSeeker_login.this, "Account with this "+t_name+ " email do not exist", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
